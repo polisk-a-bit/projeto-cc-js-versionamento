52f50fe488330558b406aaab2525cf1230ce81d7
polisk <matheusrestivolouredo@gmail.com>
2025-04-29 20:11:35 -0300

    Delete autenticacao.js

--- autenticacao.js
+++
@@ -1,7 +0,0 @@
-function autenticarUsuario(email, senha) {
- if (email === "aluno@exemplo.com" && senha === "senha123") {
- return "Autenticação bem-sucedida";
- }
- return "Falha na autenticação";
-}
-module.exports = { autenticarUsuario };
