cd4c8824ce20957c64267368449c7093ee9d1a79
Mateus Restivo Lourêdo - 102410779 <102410779@ulife.com.br>
2025-04-29 20:24:18 -0300

    Atualiza mensagens de autenticação

--- autenticacao.js
+++ autenticacao.js
@@ -1,7 +1,8 @@
 function autenticarUsuario(email, senha) {
- if (email === "aluno@exemplo.com" && senha === "senha123") {
- return "Autenticação bem-sucedida";
+ if (email === "aluno@exemplo.com" && senha === "senha123")
+{
+ return "Login bem-sucedido";
  }
- return "Falha na autenticação";
+ return "Erro no login";
 }
 module.exports = { autenticarUsuario };
